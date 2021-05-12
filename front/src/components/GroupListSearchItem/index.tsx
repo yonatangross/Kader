4f042eeb80a7361b98a3ee205112752d38f21bd6
yonatangross <yonatan2gross@gmail.com>
2021-05-12 18:03:18 +0300

    added grouplistsearchItem

---
+++ front/src/components/GroupListSearchItem/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Avatar, Button, ListItem } from '@ui-kitten/components';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export interface GroupListSearchItemProps {
+  group: any;
+}
+
+const GroupListSearchItem = (props: GroupListSearchItemProps) => {
+  let { group: group } = props;
+
+  const navigation = useNavigation();
+
+  const renderItemAccessory = (props: any) => (
+    <Button size="tiny">
+      Go to Group
+    </Button>
+  );
+
+  const renderItemIcon = (props: any) => (
+    <Avatar {...props} style={styles.profileAvatar} size="large" source={require('../../layouts/social/profile/assets/image-profile-1.jpg')} />
+  );
+
+  const onClick = () => {
+    navigation.navigate('SingleGroup', {
+      id: group.groupId,
+    });
+  };
+
+  return (
+    <TouchableOpacity onPress={onClick}>
+      <ListItem
+        title={`${group.name} - ${group.category}`}
+        description={`${group.membersCount} members, ${group.postsCount} posts,`}
+        accessoryLeft={renderItemIcon}
+        accessoryRight={renderItemAccessory}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileAvatar: {
+    marginHorizontal: 8,
+  },
+  goToGroupButton: {
+    color: 'black',
+  },
+});
+
+export default GroupListSearchItem;
